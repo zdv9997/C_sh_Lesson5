08a55cdde64e0475c19a22ca4ffaa8df2b9d550f
Dmitriy <zdv9997@mail.ru>
2022-11-27 20:10:29 +0200

    difference between largest anl smallest

---
+++ zad38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+float[] array = new float[10];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-100, 100);
+Console.WriteLine("Выводим массив: ");
+foreach (int n in array)
+    Console.WriteLine(n);
+float min = array[0];
+float max = array[0];
+foreach (int n in array)
+{
+    if (n < min) min = n;
+    if (n > max) max = n;
+}
+Console.WriteLine($"Самое большое число: {max}, самое маленькое число: {min}, разница между ними равна: {max - min}");
+
