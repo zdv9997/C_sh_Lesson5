91e6e21d706024df9f6e712f3ed358aa71a1ff35
Dmitriy <zdv9997@mail.ru>
2022-11-27 16:15:05 +0200

    считаем количество четных чисел

---
+++ zad34/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+}
+int count = 0;
+Console.WriteLine("Четные числа массива:");
+foreach (int n in array)
+{
+    if (n % 2 == 0)
+    {
+        count = count + 1;
+        Console.Write($"{n} ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных числе в массиве: {count}");
+string str = string.Join(",", array);
+Console.WriteLine($"Весь массив: {str}");
